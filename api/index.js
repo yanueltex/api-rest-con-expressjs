543350e2a0de5fc299bbd8f0eff274fa96edabe5
yleal <63595588+yleal@users.noreply.github.com>
2024-06-17 18:11:53 -0400

    Deploy

--- api/index.js
+++ api/index.js
@@ -9,14 +9,14 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const whitelist = ['http://localhost:8080', 'https://myapp.com'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin || !origin)) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
@@ -25,11 +25,11 @@ const options = {
 };
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
